10a70541e5e17f1aa513060b6ab38551a89c257c
Tanvir Al Hasib <tanviralhasib1@gmail.com>
2026-07-31 11:43:03 +0600

    created custom card widget

---
+++ lib/widgets/cardWidget.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Cardwidget extends StatelessWidget {
+  const Cardwidget({super.key, required String this.text});
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Theme.of(context).cardColor,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadiusGeometry.circular(18)),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 14),
+        child: Text(text, style:TextStyle(
+          fontSize: 16,
+        ),
+        ),
+      ),
+    );
+  }
+}
