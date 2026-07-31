b577db6132c752b757c972e0e384b0153da11996
Tanvir Al Hasib <tanviralhasib1@gmail.com>
2026-07-31 12:37:34 +0600

    theme updated

--- lib/themes/light.dart
+++ lib/themes/light.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 ThemeData lightTheme = ThemeData(
   brightness: Brightness.light,
-  scaffoldBackgroundColor: Colors.white,
+  scaffoldBackgroundColor: Color(0XFFF8F9FF),
+  inputDecorationTheme: InputDecorationTheme(
+    fillColor: Colors.white,
+  ),
+  cardColor: Color(0XFFE5EEFF),
+  textTheme: GoogleFonts.interTextTheme(),
 );
