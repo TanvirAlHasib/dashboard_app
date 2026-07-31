8c2f2f2247a655642546086e31625705998b2d32
Tanvir Al Hasib <tanviralhasib1@gmail.com>
2026-07-31 10:25:37 +0600

    GoRouter configured initially

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,3 +1,21 @@
-class AppRoutes {
+import 'package:dashboard/screens/home_screen.dart';
+import 'package:dashboard/screens/note_screen.dart';
+import 'package:go_router/go_router.dart';
 
+class AppRoutes {
+  GoRouter appRoute = GoRouter(
+    initialLocation: "note",
+      routes: [
+        GoRoute(
+          path: "/",
+          name: "home",
+          builder: (context, state) => HomeScreen(),
+        ),
+        GoRoute(
+          path: "",
+          name: "note",
+          builder: (context, state) => NoteScreen(),
+        ),
+      ],
+  );
 }
