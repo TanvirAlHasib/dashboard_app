c267318ebd30d5ac9ab6ff2231fdf8760d0eb46e
Tanvir Al Hasib <tanviralhasib1@gmail.com>
2026-08-01 22:50:31 +0600

    added factory constructor to convert map to note model list

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,8 +1,10 @@
+import 'package:dashboard/database/db_helper.dart';
+
 class NoteModel {
-  NoteModel({required this.title, required this.description, this.category = ""});
+  NoteModel({required this.title, required this.description, this.category = "", this.dateTime});
 
   final String category;
-  final DateTime ? dateTime = null;
+  final DateTime ? dateTime;
   final String title;
   final String description;
 
@@ -10,5 +12,14 @@ class NoteModel {
   String get Description =>  description;
   String get Category =>  category;
   DateTime get Datetime => dateTime!;
+
+  factory NoteModel.fromMap(Map<String, dynamic> map){
+    return NoteModel(
+      title: map[DbHelper.TITLE],
+      description: map[DbHelper.DESCRIPTION],
+      category: map[DbHelper.CATEGORY],
+      dateTime: map[DbHelper.DATE_TIME]
+    );
+  }
 
 }
