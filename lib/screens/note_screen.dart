1ac7f7f4503a20b1b3bfeef314b6f04dc59e4eae
Tanvir Al Hasib <tanviralhasib1@gmail.com>
2026-08-02 23:14:51 +0600

    made functional by adding consumer, ListView.builder and many more

--- lib/screens/note_screen.dart
+++ lib/screens/note_screen.dart
@@ -1,9 +1,11 @@
+import 'package:dashboard/models/note_model.dart';
+import 'package:dashboard/utils/provider_state_management.dart';
 import 'package:dashboard/widgets/cardWidget.dart';
 import 'package:dashboard/widgets/floatingActionButtonWidget.dart';
 import 'package:dashboard/widgets/textFormFieldWidget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 class NoteScreen extends StatelessWidget {
   NoteScreen({super.key});
@@ -20,7 +22,9 @@ class NoteScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: .start,
           children: [
-            Textformfieldwidget(hintText: "Search notes...", icon: CupertinoIcons.search),
+            Textformfieldwidget(hintText: "Search notes...",
+              icon: CupertinoIcons.search, textEditingController: searchController,
+            ),
 
             const SizedBox(
               height: 20,
@@ -43,80 +47,104 @@ class NoteScreen extends StatelessWidget {
               height: 25,
             ),
 
-            Container(
-              padding: EdgeInsets.all(15),
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(15),
-                border: Border.all(
-                  color: Colors.grey.shade300,
-                  strokeAlign: BorderSide.strokeAlignOutside
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(15),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(
+                    color: Colors.grey.shade300,
+                    strokeAlign: BorderSide.strokeAlignOutside
+                  )
+                ),
+                child: Consumer<ProviderStateManagement>(builder: (_, provider, _) {
+
+                  final List<NoteModel> noteList = provider.getNoteList;
+
+                  if(noteList.isEmpty){
+                    return Center(
+                      child: Text(
+                        "No notes yet"
+                      ),
+                    );
+                  }
+
+                  return ListView.builder(
+                    itemCount: noteList.length,
+                    itemBuilder: (_, int index) {
+                      return Column(
+                        crossAxisAlignment: .start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: .spaceBetween,
+                            children: [
+                              Card(
+                                color: Theme.of(context).cardColor,
+                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(3)),
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 5),
+                                  child: Text(noteList[index].dateTime, style: TextStyle(
+                                      fontSize: 13
+                                  ),
+                                  ),
+                                ),
+                              ),
+                              Card(
+                                color: Theme.of(context).cardColor,
+                                elevation: 0,
+                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(14)),
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5),
+                                  child: Text(noteList[index].category, style: TextStyle(
+                                      fontSize: 13,
+                                      color: Colors.green,
+                                      fontWeight: FontWeight(600)
+                                  ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Text(noteList[index].title, style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight(600)
+                          ),
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Text(noteList[index].description),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          Row(
+                            mainAxisAlignment: .spaceBetween,
+                            children: [
+                              IconButton(
+                                  onPressed: () {},
+                                  icon: Icon(Icons.delete_forever_rounded, color: Colors.red, size: 31,)
+                              ),
+                              IconButton(
+                                  onPressed: () {},
+                                  icon: Icon(Icons.edit_note_outlined, color: Colors.green.shade500, size: 34,)
+                              ),
+                            ],
+                          ),
+                          Divider(),
+                          const SizedBox(
+                            height: 10,
+                          )
+                        ],
+                      );
+                    },
+                  );
+                },
                 )
-              ),
-              child: Column(
-                crossAxisAlignment: .start,
-                children: [
-                  Row(
-                    mainAxisAlignment: .spaceBetween,
-                    children: [
-                      Card(
-                        color: Theme.of(context).cardColor,
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(3)),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 5),
-                          child: Text("May, 10", style: TextStyle(
-                              fontSize: 13
-                          ),
-                          ),
-                        ),
-                      ),
-                      Card(
-                        color: Theme.of(context).cardColor,
-                        elevation: 0,
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(14)),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5),
-                          child: Text("Work", style: TextStyle(
-                            fontSize: 13,
-                            color: Colors.green,
-                            fontWeight: FontWeight(600)
-                          ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Text("Client Meeting Notes", style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight(600)
-                  ),
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  Text("Client requested an updated timeline by Wednesday. They prefer a darker color palette for the next iteration of the prototype.",
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  Row(
-                    mainAxisAlignment: .spaceBetween,
-                    children: [
-                      IconButton(
-                          onPressed: () {},
-                          icon: Icon(Icons.delete_forever_outlined, color: Colors.red, size: 34,)
-                      ),
-                      IconButton(
-                          onPressed: () {},
-                          icon: Icon(Icons.edit_note_outlined, color: Colors.green.shade500, size: 34,)
-                      ),
-                    ],
-                  ),
-                ],
               ),
             ),
           ],
