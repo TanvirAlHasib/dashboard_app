64cc706258c309587832bcdf1b099e21020c8edc
Tanvir Al Hasib <tanviralhasib1@gmail.com>
2026-08-07 00:07:50 +0600

    added factory constructor to convert map to TaskModel

--- lib/models/task_model.dart
+++ lib/models/task_model.dart
@@ -1,3 +1,5 @@
+import 'package:dashboard/database/task_db_helper.dart';
+
 class TaskModel {
   const TaskModel({required this.title, required this.subTitle,
     this.totalTask, this.completedTask, this.completed, this.dateTime
@@ -8,4 +10,16 @@ class TaskModel {
   final String title;
   final String subTitle;
   final String ? dateTime;
+
+  // factory constructor to convert map to TaskModel
+  factory TaskModel.fromMap(Map<String, dynamic> map){
+    return TaskModel(
+      title: map[TaskDbHelper.TITLE_COLUMN],
+      subTitle: map[TaskDbHelper.SUBTITLE_COLUMN],
+      totalTask: map[TaskDbHelper.TOTAL_TASK_COLUMN],
+      completedTask: map[TaskDbHelper.TOTAL_TASK_COMPLETED_COLUMN],
+      completed: map[TaskDbHelper.COMPLETED_OR_NOT_COLUMN],
+      dateTime: map[TaskDbHelper.DATE_TIME_COLUMN],
+    );
+  }
 }
