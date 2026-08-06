eb57e1e80faeb34026cdb688721979435ad0a6ef
Tanvir Al Hasib <tanviralhasib1@gmail.com>
2026-08-06 11:42:45 +0600

    added action button to the snackbar to confirm deletion of particular note

--- lib/utils/toast.dart
+++ lib/utils/toast.dart
@@ -5,9 +5,13 @@ class Toast {
   Toast(BuildContext this.context);
   BuildContext context;
   // function to show snackBar
-  void show(String message){
+  void show(String message, VoidCallback function){
     final SnackBar snackBar = SnackBar(
       content: Text(message),
+      action: SnackBarAction(
+        label: "Yes",
+        onPressed: function
+      ),
     );
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
