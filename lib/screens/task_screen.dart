a3ac024389c4ac5c7b049b664de65b0854ad6421
Tanvir Al Hasib <tanviralhasib1@gmail.com>
2026-08-06 18:27:06 +0600

    task complete circular progress design done

--- lib/screens/task_screen.dart
+++ lib/screens/task_screen.dart
@@ -9,6 +9,60 @@ class TaskScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       appBar: MyAppBar(context),
+      body: Container(
+        padding: const EdgeInsets.all(13),
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(17),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Color(0XFFE5EEFF),
+              ),
+              child: Row(
+                spacing: 25,
+                children: [
+                  Stack(
+                    alignment: AlignmentGeometry.center,
+                    children: [
+                      Text(
+                        "40%",
+                        style: TextStyle(
+                          color: Colors.blue.shade900,
+                          fontWeight: FontWeight(600),
+                        ),
+                      ),
+                      CircularProgressIndicator.adaptive(
+                        padding: EdgeInsets.only(bottom: 10,right: 10, top: 10, left: 15),
+                        value: 0.4,
+                        strokeAlign: 4,
+                        strokeWidth: 6,
+                        backgroundColor: Colors.blue.shade100,
+                        valueColor: AlwaysStoppedAnimation(Colors.blue.shade700),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: .start,
+                    children: [
+                      Text(
+                        "4/10",
+                        style: TextStyle(
+                          fontWeight: FontWeight(500),
+                          fontSize: 21,
+                        ),
+                      ),
+                      Text(
+                        "Tasks completed",
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
