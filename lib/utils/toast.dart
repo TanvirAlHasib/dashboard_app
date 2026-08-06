db7ea43f8d6ae9236bdeb96b006095c75ccbd5c6
Tanvir Al Hasib <tanviralhasib1@gmail.com>
2026-08-06 11:58:20 +0600

    basic design to the snackbar

--- lib/utils/toast.dart
+++ lib/utils/toast.dart
@@ -7,11 +7,17 @@ class Toast {
   // function to show snackBar
   void show(String message, VoidCallback function){
     final SnackBar snackBar = SnackBar(
-      content: Text(message),
+      content: Text(message, style: TextStyle(color: Colors.black),),
       action: SnackBarAction(
         label: "Yes",
-        onPressed: function
+        onPressed: function,
+        textColor: Colors.black,
       ),
+      backgroundColor: Theme.of(context).cardColor,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadiusGeometry.circular(8)),
+      behavior: SnackBarBehavior.floating,
+      showCloseIcon: true,
+      closeIconColor: Colors.black,
     );
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
