6099cab3e4f2b0d47e811a49a0474d240dcc99be
Tanvir Al Hasib <tanviralhasib1@gmail.com>
2026-08-12 13:46:57 +0600

    updated toast message

--- lib/screens/note_screen.dart
+++ lib/screens/note_screen.dart
@@ -153,7 +153,7 @@ class NoteScreen extends StatelessWidget {
                                   onPressed: () {
                                     int id = noteList[index].id;
                                     Toast(context).show(
-                                      "Want to delete note ?",
+                                      "Do you want to delete the note ?",
                                       () async => await context.read<ProviderStateManagement>().deleteNote(id),
                                     );
                                   },
