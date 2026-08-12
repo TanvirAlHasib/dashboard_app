bea91376bcd05249489444a61cef3ae9e8161ca2
Tanvir Al Hasib <tanviralhasib1@gmail.com>
2026-08-12 19:27:40 +0600

    added app bar

--- lib/screens/expenses_screen.dart
+++ lib/screens/expenses_screen.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class ExpensesScreen extends StatelessWidget {
   const ExpensesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      appBar: MyAppBar(context),
+    );
+  }
+
+  // app bar
+  AppBar MyAppBar(BuildContext context){
+    return AppBar(
+      elevation: 3,
+      title: Text("Expenses", style: TextStyle(fontSize: 19),),
+      centerTitle: true,
+      backgroundColor: Theme.of(context).cardColor,
+    );
   }
 }
