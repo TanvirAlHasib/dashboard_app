a900879b26bd36f3ee2d2625ab2591edb2924be9
Tanvir Al Hasib <tanviralhasib1@gmail.com>
2026-08-13 15:36:43 +0600

    added expense model

---
+++ lib/models/expense_model.dart
@@ -0,0 +1,25 @@
+import 'package:dashboard/database/expense_db_helper.dart';
+
+class ExpenseModel {
+
+  ExpenseModel({ required this.title, required this.expense,
+    required this.budget, required this.category, this.id
+  });
+
+  final int ? id;
+  final String title;
+  final int expense;
+  final String category;
+  final int budget;
+
+  factory ExpenseModel.fromMap(Map<String, dynamic> map){
+    return ExpenseModel(
+      title: map[ExpenseDbHelper.TITLE_COLUMN],
+      expense: map[ExpenseDbHelper.EXPENSES_COLUMN],
+      budget: map[ExpenseDbHelper.BUDGET_COLUMN],
+      category: map[ExpenseDbHelper.CATEGORY_COLUMN],
+      id: map[ExpenseDbHelper.ID_COLUMN]
+    );
+  }
+
+}
