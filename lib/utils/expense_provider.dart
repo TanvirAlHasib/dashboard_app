9027c28e3017abd5cad7f7df25b1da997d30fccb
Tanvir Al Hasib <tanviralhasib1@gmail.com>
2026-08-13 15:52:46 +0600

    added fetch expense event in provider

--- lib/utils/expense_provider.dart
+++ lib/utils/expense_provider.dart
@@ -1,5 +1,19 @@
+import 'package:dashboard/database/expense_db_helper.dart';
+import 'package:dashboard/models/expense_model.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:sqflite/sqflite.dart';
 
 class ExpenseProvider extends ChangeNotifier {
+  final List<ExpenseModel> expensesList = [];
+  List<ExpenseModel> get getExpensesList => expensesList;
 
+  Future<void> getAllExpenses() async{
+    Database database = await ExpenseDbHelper.getInstance.getExpenseDB();
+    expensesList.clear();
+    List<Map<String, dynamic>> mapExpenses = await database.query(ExpenseDbHelper.TABLE_NAME);
+    mapExpenses.forEach((expense) {
+      expensesList.add(ExpenseModel.fromMap(expense));
+    },);
+    notifyListeners();
+  }
 }
