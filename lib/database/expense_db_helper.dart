2822f6290c26054d4760fcb1d78d056ba2ca334c
Tanvir Al Hasib <tanviralhasib1@gmail.com>
2026-08-13 15:26:32 +0600

    added expense database helper class

---
+++ lib/database/expense_db_helper.dart
@@ -0,0 +1,51 @@
+import 'dart:io';
+
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+
+class ExpenseDbHelper {
+
+  ExpenseDbHelper._();
+  static final ExpenseDbHelper getInstance = ExpenseDbHelper._();
+  static final String TABLE_NAME = "expense";
+  static final String DB_NAME = "expenses.db";
+  static final String ID_COLUMN = "id";
+  static final String EXPENSES_COLUMN = "expenses";
+  static final String TITLE_COLUMN  = "title";
+  static final String CATEGORY_COLUMN = "category";
+  static final String BUDGET_COLUMN = "budget";
+  Database ? db;
+
+  Future<Database> getExpenseDB() async{
+    if(db != null){
+      return db!;
+    } else {
+      db = await getDB();
+      return db!;
+    }
+  }
+
+  Future<Database> getDB() async{
+    Directory databasePath = await getApplicationDocumentsDirectory();
+    String path = join(databasePath.path, DB_NAME);
+    return db = await openDatabase(
+      path,
+      onCreate: (db, version) {
+        return db.execute(
+          '''
+          create table $TABLE_NAME(
+          $ID_COLUMN INTEGER PRIMARY KEY AUTOINCREMENT,
+          $TITLE_COLUMN text not null,
+          $EXPENSES_COLUMN INTEGER not null,
+          $CATEGORY_COLUMN text not null,
+          $BUDGET_COLUMN INTEGER not null
+          )
+          '''
+        );
+      },
+      version: 1
+    );
+  }
+
+}
