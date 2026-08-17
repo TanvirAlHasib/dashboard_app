7619dfde73cf9550be4d1210b7411ac2a5c47fe9
Tanvir Al Hasib <tanviralhasib1@gmail.com>
2026-08-17 09:41:50 +0600

    app bar design done

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,7 +7,21 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: MyAppBar(context),
+    );
+  }
 
+  // appbar starts here
+  AppBar MyAppBar(BuildContext context){
+    return AppBar(
+      backgroundColor: Theme.of(context).cardColor,
+      elevation: 3,
+      centerTitle: true,
+      title: Text("Dashboard", style: TextStyle(
+        //fontWeight: FontWeight(600),
+        fontSize: 19,
+      ),
+      ),
     );
   }
 }
