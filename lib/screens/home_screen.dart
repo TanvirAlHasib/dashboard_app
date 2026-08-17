ae6903211c5c8307bbf541e4ab5a73bc42705b4d
Tanvir Al Hasib <tanviralhasib1@gmail.com>
2026-08-17 12:04:01 +0600

    dashBoard info design done

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -36,7 +36,56 @@ class HomeScreen extends StatelessWidget {
                   },)
                 ],
               ),
-            )
+            ),
+            Text("Dashboard Info", style: TextStyle(
+                fontSize: 21,
+                fontWeight: FontWeight(600)
+            ),),
+            Row(
+              spacing: 5,
+              children: [
+                getTaskNoteInfo(info: "5", title: "Today's Task", icon: Icons.add_task, subTitle: "remaining"),
+                getTaskNoteInfo(info: "3", title: "Quick Notes", icon: Icons.sticky_note_2_sharp, subTitle: "recent"),
+              ],
+            ),
+            Card(
+              color: Colors.white,
+              elevation: 0,
+              shape: RoundedRectangleBorder(side: BorderSide(
+                  color: Colors.blue.shade200
+              ), borderRadius: BorderRadiusGeometry.circular(10)),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                child: Row(
+                  spacing: 10,
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.blue.shade50,
+                      maxRadius: 25,
+                      child: Icon(Icons.money, color: Colors.blue.shade800,),
+                    ),
+                    Column(
+                      spacing: 1,
+                      children: [
+                        Text("Monthly Expenses", style: TextStyle(
+                          fontSize: 16,
+                        ),),
+                        Row(
+                          spacing: 8,
+                          children: [
+                            Text("4000", style: TextStyle(
+                                fontSize: 25,
+                                fontWeight: FontWeight(600)
+                            ),),
+                            Text("taka spent")
+                          ],
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
@@ -57,6 +106,7 @@ class HomeScreen extends StatelessWidget {
     );
   }
 
+  // Quick action event
   Widget getQuickActions({required String actionName, required IconData icon, required Function action}){
     return InkWell(
       onTap: () {
@@ -81,6 +131,50 @@ class HomeScreen extends StatelessWidget {
               Text(actionName, style: TextStyle(
                   fontWeight: FontWeight(500)
               ),)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  // get task and note info
+  Widget getTaskNoteInfo({required String info, required String title, required IconData icon, required String subTitle}){
+    return Expanded(
+      child: Card(
+        color: Colors.white,
+        elevation: 0,
+        shape: RoundedRectangleBorder(side: BorderSide(
+            color: Colors.blue.shade200
+        ), borderRadius: BorderRadiusGeometry.circular(10)),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 9.0, horizontal: 20),
+          child: Column(
+            crossAxisAlignment: .start,
+            spacing: 5,
+            children: [
+              CircleAvatar(
+                backgroundColor: Colors.blue.shade50,
+                maxRadius: 25,
+                child: Icon(icon, color: Colors.blue.shade800,),
+              ),
+              const SizedBox(
+                height: 1,
+              ),
+              Text(title, style: TextStyle(
+                fontSize: 16,
+              ),),
+              Row(
+                mainAxisSize: .min,
+                spacing: 8,
+                children: [
+                  Text(info, style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight(600)
+                  ),),
+                  Text(subTitle,)
+                ],
+              )
             ],
           ),
         ),
